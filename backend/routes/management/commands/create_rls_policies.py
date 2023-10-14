c8c022ae25cdd027f44477ce13acf45093b86bfa
JJStoker <jeroen229@gmail.com>
2023-10-14 21:19:43 +0200

    Create team policies via claims

--- backend/routes/management/commands/create_rls_policies.py
+++ backend/routes/management/commands/create_rls_policies.py
@@ -12,20 +12,15 @@ from django.db import connection
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    def __init__(self, *args, **kwargs):
-        super(Command, self).__init__(*args, **kwargs)
-
-    def add_arguments(self, parser):
-        parser.add_argument('--create-role', action="store_true")
-        parser.add_argument('--create-type', action="store_true")
-        pass
-
     def create_roles(self, cursor):
-        role_name = "user_role"
-        cursor.execute(f"""
-            DROP ROLE IF EXISTS {role_name};
-            CREATE ROLE {role_name};
-        """)
+        for role_name in ["user_role", "team_role"]:
+            try:
+                cursor.execute(f"""
+                    DROP ROLE IF EXISTS {role_name};
+                    CREATE ROLE {role_name};
+                """)
+            except:
+                print("role already created " + role_name)
 
     def create_type(self, cursor):
         cursor.execute(f"""
@@ -34,7 +29,8 @@ class Command(BaseCommand):
                 aud text,
                 role text,
                 exp integer,
-                user_id integer
+                user_id integer,
+                team_ids integer[]
             );
             """)
 
@@ -83,10 +79,57 @@ class Command(BaseCommand):
             except Exception as e:
                 self.stderr.write(self.style.ERROR(f'Error creating RLS for {table_name}: {str(e)}'))
 
+    def create_team_roles(self, cursor):
+        # Create RLS Policies
+        role_name = "user_role"
+        cursor.execute(f"""
+            CREATE OR REPLACE FUNCTION current_team_ids() RETURNS integer[] AS $$
+            BEGIN
+                RETURN string_to_array(nullif(current_setting('jwt.claims.team_ids', true), ''), ',')::integer[];
+            END;
+            $$ LANGUAGE plpgsql STABLE;
+        """)
+        cursor.execute(f"""
+        GRANT EXECUTE ON FUNCTION current_team_ids TO {role_name};
+        """)
+
+        for model in [cls for cls in Model.__subclasses__() if hasattr(cls, "team")]:
+            table_name = model._meta.db_table
+            policy_name = f"{role_name}_team_rls"
+
+            # RLS Policy SQL
+            rls_policy_sql = f"""
+            GRANT ALL ON TABLE {table_name} TO {role_name};
+            CREATE POLICY {policy_name} ON {table_name}
+            FOR SELECT TO {role_name} 
+            USING ("team_id" = ANY(current_team_ids()));
+            """
+
+            # Activating RLS SQL
+            activate_rls_sql = f"""
+            ALTER TABLE {table_name} ENABLE ROW LEVEL SECURITY;
+            """
+            
+            # Debugging: Print the SQL being executed
+            print("Executing Activation SQL: ", activate_rls_sql.strip())
+            print("Executing RLS Policy SQL: ", rls_policy_sql.strip())
+            
+            # Execute SQL
+            try:
+                cursor.execute(f"SELECT 1 FROM pg_policies WHERE schemaname = 'public' AND policyname = '{policy_name}' AND tablename = '{table_name}';")
+                if cursor.fetchone():
+                    cursor.execute(f"DROP POLICY {policy_name} on {table_name};")
+                cursor.execute(rls_policy_sql)
+                cursor.execute(activate_rls_sql)
+                self.stdout.write(self.style.SUCCESS(f'Successfully created RLS for {table_name}'))
+            except Exception as e:
+                self.stderr.write(self.style.ERROR(f'Error creating RLS for {table_name}: {str(e)}'))
+
     def handle(self, *args, **options):
         print (options)
         
         with connection.cursor() as cursor:
-            if options.get("create_role"): self.create_roles(cursor)
-            if options.get("create_type"): self.create_type(cursor)
+            self.create_roles(cursor)
+            self.create_type(cursor)
             self.create_user_roles(cursor)
+            self.create_team_roles(cursor)
